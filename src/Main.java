a70c0bce4176c4a61335ecc0098ca140c3b3d15a
Daylen Jones <jonesday001@warren.k12.in.us>
2025-03-07 12:55:41 -0500

    ud

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class FilterOddNumbers {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+
+        List<Integer> oddNumbers = numbers.stream()
+                .filter(n -> n % 2 != 0)
+                .collect(Collectors.toList());
+
+
+        System.out.println("Odd numbers: " + oddNumbers);
+    }
 }
